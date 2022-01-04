49680376d3e101468eb10de9a128052a12093342
Lorena C Silva <loricristinesilva@gmail.com>
2022-01-04 20:41:23 -0300

    RPA - Correios all project

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using OpenQA.Selenium.Chrome;
+using RPACorreios;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string url = "https://buscacepinter.correios.com.br/app/endereco/index.php";
+
+        List<string> numeroCEP = new List<string>()
+        {
+            "17014900" // Preencher com o valor do CEP desejado, nesse caso estou utilizando da prefeitura de Bauru
+        };
+
+        ChromeDriver driver = new ChromeDriver();
+        driver.Navigate().GoToUrl(url);
+        driver.Manage().Window.Maximize();
+
+        foreach (var endereco in numeroCEP)
+        {
+            var pesquisaEl = driver.FindElement(OpenQA.Selenium.By.Id("endereco"));
+            pesquisaEl.SendKeys(endereco);
+
+            var btnBuscar = driver.FindElement(OpenQA.Selenium.By.XPath($"//button[@id='btn_pesquisar']"));
+            btnBuscar.Click();
+
+            Thread.Sleep(3000);
+
+            // Atribuindo valores da tabela de pesquisa para as variáveis 
+
+            var logradouroValue = driver.FindElement(OpenQA.Selenium.By.XPath("//*[@id='resultado-DNEC\']/tbody/tr/td[1]"));
+            var bairroValue = driver.FindElement(OpenQA.Selenium.By.XPath("//*[@id='resultado-DNEC\']/tbody/tr/td[2]"));
+            var localidadeValue = driver.FindElement(OpenQA.Selenium.By.XPath("//*[@id='resultado-DNEC\']/tbody/tr/td[3]"));
+            var cepValue = driver.FindElement(OpenQA.Selenium.By.XPath("//*[@id='resultado-DNEC\']/tbody/tr/td[4]"));
+
+            SendExcel excelValues = new SendExcel();
+
+            excelValues.retornoTabela(logradouroValue.Text, bairroValue.Text, localidadeValue.Text, cepValue.Text);
+        }
+    }
+}
---
+++ SendExcel.cs
@@ -0,0 +1,30 @@
+﻿using ClosedXML.Excel;
+using System.Diagnostics;
+namespace RPACorreios
+{
+    public class SendExcel
+    {
+        public void retornoTabela(string logradouro, string bairro, string localidade, string CEP)
+        {
+            using (var workbok = new XLWorkbook())
+            {
+                var worksheet = workbok.Worksheets.Add("Planilha1");
+
+                worksheet.Cell("A1").Value = "Logradouro/Nome";
+                worksheet.Cell("B1").Value = "Bairro/Distrito";
+                worksheet.Cell("C1").Value = "Localidade/UF";
+                worksheet.Cell("D1").Value = "CEP";
+
+                worksheet.Cell("A2").Value = logradouro;
+                worksheet.Cell("B2").Value = bairro;
+                worksheet.Cell("C2").Value = localidade;
+                worksheet.Cell("D2").Value = CEP;
+
+                workbok.SaveAs(@"c:\temp\RPACorreios.xlsx");
+            }
+
+            Process.Start(new ProcessStartInfo(@"c:\temp\RPACorreios.xlsx") { UseShellExecute = true }); //Esse comando chama o programa padrão que o computador utiliza para abrir arquivos xlsx
+
+        }
+    }
+}
